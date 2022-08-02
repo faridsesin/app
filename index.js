3e78fde2f0ce16987a851724552ed756d7b64865
fsesin <faridsesin@gmail.com>
2022-08-02 15:10:28 -0500

    primera prueba

---
+++ index.js
@@ -0,0 +1,48 @@
+/*
+Diapositiva 4 - 8
+CONTROL DE VERSIONES
+Vamos a hablar acerca del control de versiones. 
+Que no es mas que la forma en que gestionamos los cambios en el codigo fuente a lo largo 
+del tiempo. Para cualquier equipo de desarrollo, el codigo principal es extremadamente 
+importante, por lo que el control de versiones proteje a este codigo de accidente o 
+errores humanos que puedan afectarlo especificamente.
+Nosotros como desarrolladores, estamos escribiendo codigo de manera continua, cada
+uno de nosotros se encuentra trabajando en una funcionalidad distinta, si nosotros 
+estuvieramos ingresando los cambios de manera directa al codigo fuente la cantidad de errores
+y sobreescrituras que presentariamos en un proyecto son inimaginables. Es aqui donde entra
+en juego el control de versiones, ya que realiza un seguimiente de todos los cambios individuales
+de cada desarrollador, para evitar los conflictos. 
+
+Esto nos permite mantener eficiencia sin importar que tanto escale nuestro desarrollo.
+Para ser mas puntuales, las ventajas de tener un control de versiones son:
+- tenemos un historial de cambios de cada uno de nuestros archivos sin importar
+el tiempo que pase.
+- podemos crear ramas, manteniendo flujos de trabajo independientes y con la posibilidad
+de fusionarlos cuando querramos
+
+
+Diapositiva 9 - 27
+GIT
+Ahora bien, el sistema de control de versiones mas usado y conocido por todos nosotros es
+GIT. Es de codigo abierto y con una gestion de mantenimiento constante. y funciona perfectamente
+con cualquier entorno de desarrollo integrado como VSC.
+
+Entre las alternativas a GIT tenemos:
+-apache subversion
+mercurial
+-azure devops server
+
+
+
+Diapositiva 28 - 31
+DESAFIO GIT
+
+Diapositiva 34 - 39
+PAAS
+
+Diapositivas 40 - 61
+HEROKU
+
+Diapositivas 62 - 64 
+DESAFIO HEROKU
+*/
